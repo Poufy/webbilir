72abb8b491d51e4f2dbec71d5a9ee8d8a432b524
Noor Aldeen Al Fayad <nooraldeenalfayad@hotmail.com>
2020-01-08 17:53:23 +0300

    setup the database with ms sql server

---
+++ webbilirAPI/Migrations/20200108131110_AddStudenttoDB.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace webbilirAPI.Migrations
+{
+    public partial class AddStudenttoDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    classId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentItems");
+        }
+    }
+}
--- webbilirAPI/Startup.cs
+++ webbilirAPI/Startup.cs
@@ -8,15 +8,25 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using webbilirAPI.Models;
 
 namespace webbilirAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration {get;}
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<StudentContext>
+                (opt => opt.UseSqlServer(Configuration["Data:StudentAPIConnection:ConnectionString"]));
             //Initializing/settingup our service
             services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
