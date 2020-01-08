b7aa4cf78848bcd4666cb0c5ac899d940fd956bd
Noor Aldeen Al Fayad <nooraldeenalfayad@hotmail.com>
2020-01-08 19:31:10 +0300

    Added put/post/delete requests to student model

--- webbilirAPI/Controllers/StudentsController.cs
+++ webbilirAPI/Controllers/StudentsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using webbilirAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace webbilirAPI.Controllers
 {
@@ -29,6 +30,51 @@ namespace webbilirAPI.Controllers
             var student = _context.StudentItems.Find(id);
             if(student == null)
                 return NotFound();
+            return student;
+        }
+
+        //POST:         api/students
+        [HttpPost]
+        public ActionResult<Student> AddStudents(Student student)
+        {
+            //Add the data to a localized memory.
+            _context.StudentItems.Add(student);
+            //Save this data in our database.
+            _context.SaveChanges();
+            //Return the student we just added to the database.
+            return CreatedAtAction("GetStudent", new Student {Id = student.Id}, student);
+        }
+
+        //PUT:          api/students/<id>
+        [HttpPut("{id}")]
+        public ActionResult PutStudent(int id, Student student)
+        {
+             if(id != student.Id)
+             {
+                 return BadRequest();
+             }
+            //Mark the existing object in the local machine that it has been modified
+             _context.Entry(student).State = EntityState.Modified;
+             //Save this data in our database.
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        //DELETE:           api/students/<id>
+        [HttpDelete("{id}")]
+        public ActionResult<Student> DeleteStudent(int id)
+        {
+            //Populate the entry to be returned later
+            var student = _context.StudentItems.Find(id);
+            if(student == null)
+            {
+                return NotFound();
+            }
+
+            _context.StudentItems.Remove(student);
+            _context.SaveChanges();
+            //Return deleted item
             return student;
         }
 
